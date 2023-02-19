83274e077564ac5217061ef37eb67d36c3370064
Hldka <116027001+Hldka@users.noreply.github.com>
2023-02-19 12:55:13 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/patikaMagaza/PatikaStore.java
+++ src/main/java/patikaMagaza/PatikaStore.java
@@ -1,9 +1,34 @@
 package patikaMagaza;
 
+import java.util.Scanner;
+
 public class PatikaStore {
 
     public  void start(){
         System.out.println("**********PATIKA STORE HOSGELDINIZ***************");
         System.out.println("Magazamizin ayricaliklarina bakmak istermisiniz.....");
+        System.out.println("1 - Notebook İşlemleri\n" +
+                "2 - Cep Telefonu İşlemleri\n" +
+                "3 - Marka Listele\n" +
+                "0 - Çıkış Yap");
+        Scanner scan =new Scanner(System.in);
+        int secim=scan.nextInt();
+
+        switch (secim) {
+            case 1:
+                notebookislemleri();
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 0:break;
+        }
+    }
+
+    private void notebookislemleri() {
+
     }
 }
